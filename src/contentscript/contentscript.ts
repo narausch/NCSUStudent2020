1f33c884cf5e16ee65120e9751ae75515bdb08a4
Weston Peterson <wrpeters@ncsu.edu>
2020-02-07 11:53:04 -0500

    Changed content script inject runtime

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,7 +1,9 @@
 const element = document.getElementById('diff-0');
 const button = document.createElement('button');
 button.innerText = 'test';
-element
-    .getElementsByClassName('file-header')[0]
-    .getElementsByClassName('file-actions')[0]
-    .prepend(button);
+if (element) {
+    element
+        .getElementsByClassName('file-header')[0]
+        .getElementsByClassName('file-actions')[0]
+        .prepend(button);
+}
