81be44d8d36b541f300c6cdc1670046bffbfa548
Weston Peterson <wrpeters@ncsu.edu>
2020-02-14 16:54:17 -0500

    Completed graph tests

--- src/__tests__/graph/Graph.test.ts
+++ src/__tests__/graph/Graph.test.ts
@@ -2,7 +2,7 @@ import { Graph } from '../../contentscript/graph/Graph';
 import fs from 'fs';
 import { GraphConnection } from '../../contentscript/graph/GraphConnection';
 
-describe('Create Graph', () => {
+describe('Graph tests', () => {
     test('Constructor', async () => {
         const jsonString = fs.readFileSync('src/__tests__/resources/jsonFile1.json', 'utf8');
 
@@ -11,14 +11,44 @@ describe('Create Graph', () => {
         expect(graph.nodes.length).toBe(6);
         expect(graph.connections.length).toBe(5);
     });
+
+    test('Constructor broken object', async () => {
+        const jsonString = '{ "notConnections": "test" }';
+
+        let graph: Graph = null;
+        let result = '';
+        try {
+            graph = new Graph(jsonString);
+        } catch (err) {
+            result = (err as Error).message;
+        }
+
+        expect(result).toBe('JSON file is not formatted correctly');
+        expect(graph).toBe(null);
+    });
 });
 
-describe('Create Connection', () => {
+describe('Connection tests', () => {
     test('Constructor', async () => {
         const jsonString = '{"sourcePort": {"node": "test-id"}, "targetPort": {"node":"test-id2"}}';
         const connection = new GraphConnection(JSON.parse(jsonString));
 
         expect(connection.sourcePort).toBe('test-id');
         expect(connection.targetPort).toBe('test-id2');
+    });
+
+    test('Constructor broken', async () => {
+        const jsonString = '{"sourcePort": {"node": "test-id"}, "taetPort": {"node":"test-id2"}}';
+
+        let connection: GraphConnection = null;
+        let result = '';
+        try {
+            connection = new GraphConnection(JSON.parse(jsonString));
+        } catch (err) {
+            result = (err as Error).message;
+        }
+
+        expect(result).toBe('JSON file is not formatted correctly');
+        expect(connection).toBe(null);
     });
 });
