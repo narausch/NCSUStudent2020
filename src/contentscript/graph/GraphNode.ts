81392615e632e5ee807d62e3cff5e2c1e7fc63f6
Joel Forsyth <jsforsyt@ncsu.edu>
2020-02-25 10:38:23 -0500

    #64 Removed flag from GraphNode

--- src/contentscript/graph/GraphNode.ts
+++ src/contentscript/graph/GraphNode.ts
@@ -1,24 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-enum Flag {
-    CREATED,
-    REMOVED,
-    MODIFIED,
-    UNMODIFIED,
-}
 
 export class GraphNode {
     public id: string;
     public data: any;
-    public flag: Flag;
 
     /**
      * Constructs the GraphNode object
      * @param id The id of the graph node
      * @param data The data from the graph node
      */
-    constructor(id: string, data: any, flag?: Flag) {
+    constructor(id: string, data: any) {
         this.id = id;
         this.data = data;
-        this.flag = flag;
     }
 }
