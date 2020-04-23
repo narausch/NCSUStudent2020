d465dfba406afd1b14c0c663a752036dd94306dc
Joel Forsyth <jsforsyt@ncsu.edu>
2020-04-23 11:34:31 -0400

    Added status back

--- src/contentscript/DiffView.tsx
+++ src/contentscript/DiffView.tsx
@@ -102,6 +102,9 @@ class DiffView extends React.Component<DiffViewProps, DiffViewState> {
      * Renders the React component.
      */
     render(): React.ReactNode {
+        const bs = this.state.baseFileTotal ? this.state.baseFileTotal.toLocaleString() : '-';
+        const cs = this.state.compareFileTotal ? this.state.compareFileTotal.toLocaleString() : '-';
+
         // TODO: avoid magic numbers for VisualDiff
         return (
             <div className="fdv-view">
@@ -155,6 +158,12 @@ class DiffView extends React.Component<DiffViewProps, DiffViewState> {
                     </p>
                     <p>
                         {this.state.combinedGraph && this.state.combinedGraph.getConnStatsString()}
+                    </p>
+                    <p>
+                        Base [{bs} bytes]: {this.state.baseStatus}
+                    </p>
+                    <p>
+                        Compare [{cs} bytes]: {this.state.compareStatus}
                     </p>
                 </div>
             </div>
